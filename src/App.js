28fd0b448aa095d1134521156eb78b9782728dae
SamPrinceFranklin <samprince.franklink2020@vitstudent.ac.in>
2022-01-21 19:47:50 +0530

    deploy

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, {useState} from "react";
+import NewExpense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
+
+const DUMMY_EXPENSES = [
+  
+];
+
+function App() {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = expense => {
+    setExpenses(prevExpenses =>{
+      return [expense, ...prevExpenses];
+    });
+  };
+
+  return (
+    <div>
+    <NewExpense onAddExpense ={addExpenseHandler}/>
+    <Expenses items={expenses}/>
+    </div>
+  );
+};
+
+export default App;
